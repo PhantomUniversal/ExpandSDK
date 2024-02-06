829c3ed5816cc6aa412ec4cf6be5113abd310dfd
PhantomUniversal <chho1365@gmail.com>
2024-02-06 19:53:47 +0900

    [v0.0.1][update] * Import newtonsoft-json 3.2.1 * Add package file phantom sdk * Add phandom base helper

---
+++ SDK/Assets/Phantom/Editor/PhantomHelper.cs
@@ -0,0 +1,22 @@
+namespace PhantomEditor
+{
+    public static class PhantomHelper
+    {
+
+        /// <summary>
+        /// Identifier 
+        /// </summary>
+        internal const string Identifier = "com.phantomuniversal.sdk";
+
+        /// <summary>
+        /// Root path
+        /// </summary>
+        internal const string Root = "Phantom";
+
+        /// <summary>
+        /// Github url
+        /// </summary>
+        internal const string Url = "https://raw.githubusercontent.com/PhantomUniversal/ExpandSDK/main/Release"; // Todo 정확한 주소가 맞은지 확인 필요
+
+    }
+}
